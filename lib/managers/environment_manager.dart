7993629e410090ae2424c12e52e2e24ad5394204
דורון לוי <doronlevy66@gmail.com>
2025-11-24 17:29:56 +0200

    fixin env

--- lib/managers/environment_manager.dart
+++ lib/managers/environment_manager.dart
@@ -25,6 +25,12 @@ class EnvironmentManager {
 
   // Method to get the current API URL based on the environment
   String get apiUrl {
+    // If running on web, always use the production URL or a specific web URL
+    if (kIsWeb && kReleaseMode) {
+      // In release (deployed) web builds we always use the remote server.
+      return dotenv.env['PROD_API_URL'] ?? 'https://renderbbserver.onrender.com';
+    }
+
     switch (_currentEnvironment) {
       case Environment.LOCAL:
         return dotenv.env['LOCAL_API_URL'] ?? 'http://localhost:9090';
